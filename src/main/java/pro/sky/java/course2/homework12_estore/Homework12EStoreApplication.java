df8efeb6074e93dbe352ab1d9e7402261ef7fe45
roman <romanvafin92@gmail.com>
2023-05-29 22:10:59 +0300

    First Commit

---
+++ src/main/java/pro/sky/java/course2/homework12_estore/Homework12EStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework12_estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework12EStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework12EStoreApplication.class, args);
+    }
+
+}
